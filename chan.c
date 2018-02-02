e529868c84fc86cf9674fd152baa063809ba866b
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-02 05:52:30 -0300

    Create chan.c

---
+++ chan.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a[25],,h,p;
+float median=0,sum=0;
+printf("Enter the number of terms:");
+scanf("%d",&h);
+printf("\n Enter the number:\n");
+for(p=1;p<=h;p++)
+{
+scanf("%d",&a[p]);
+}
+for(p=1;p<=h;p++)
+{
+for(p=1;p<=h;p++)
+{
+sum=sum+a[p];
+avg=sum/n;
+}
+print("\n the median numbersare:%f",median);
+return 0;
+}
