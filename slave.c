757277b8999efdc85fadcc5a54e750b298e2202e
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-02 05:57:27 -0300

    Create slave.c

---
+++ slave.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,j,l,m,p,g;
+printf("enter the first time: ");
+scanf("%d%d",&i,&j);
+printf("enter the second time: ");
+scanf("%d%d",&l,&m);
+p=i-l;
+g=j-m;
+printf("the time is %d%d",p,f);
+return 0;
+}
