be4e3ec3ff677f84c0dd717b1dc2bc2bd64ad359
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-02 06:01:47 -0300

    Create evil.c

---
+++ evil.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a[10],z,s;
+    scanf("%d",&n);
+    for(s=0;s<z;s++)
+    {
+        scanf("%d",&a[s]);
+    }
+    for(s=0;s<z;s++)
+    {
+    printf("%d\t%d\n",a[s],s);
+    }
+    return 0;
+}
