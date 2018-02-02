f1d4870994b8b0ce264822ffeef536fdbf10de4d
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-02 06:22:18 -0300

    Create condition.c

---
+++ condition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+int i,a;
+char c[100];
+printf("enter the value");
+scanf("%c",&c);
+printf("enter the value");
+scanf("%d",i);
+for(a=0;a<=i;a++)
+printf("%c",c);
+getch();
+}
