0c1a304a1a52f6fe8923567adb7c3f9840b2117c
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-06 05:50:41 -0300

    Rename number.c to party.c

---
+++ party.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+int b,c,d,sum=0,f;
+printf("\n enter the number of values");
+scanf("%d",n);
+for(b=1;b<=c;b++)
+{
+printf("\n enter the %d value:",b);
+scanf(%d",&d);
+sum=sum+d;
+}
+c=sum/c;
+printf("\n output :",f);
+return 0;
+}
