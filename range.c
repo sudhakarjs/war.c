c35f457f7d666e57b484f74d04500af88238dd49
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-06 05:37:19 -0300

    Create range.c

---
+++ range.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+void main()
+{
+    int i;
+    printf("enter num");
+    scanf("%d",&i);
+    if((i>=1)&&(i<=10))
+    {
+    printf("range");
+    }
+    else
+    {
+        printf("not in range");
+    }
+}
