56122faccd068344aaaa711212d253f951b59206
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-06 05:31:39 -0300

    Create string3.c

---
+++ string3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str1[s1],str2[s2],a,b;
+printf("\n enter the first string");
+scanf("%s",str1);
+printf("\n enter the second string");
+scanf("%s",str2");
+int a =strlen(s1);
+int b= strlen(s2);
+if(a==b)
+{
+printf("string is equal");
+}
+if(a>b);
+{
+printf("str1  is greater");
+}
+else
+{
+printf("str2 is greater");
+}
+}
