943593c598f927bb99f1d699a35e5c3906185d7c
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-06 06:11:11 -0300

    Create power 3.c

---
+++ power 3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int j,a,c=0;
+    printf("\nEnter a number");
+    scanf("%d",&n);
+    for(a=0;a<j;a++)
+    {
+        if(pow(2,a)==j)
+        {
+           c=1; 
+        }
+    }if(c==1){printf("\n yes");}
+    else
+    {
+    	printf("\n no");
+    }
+
+    return 0;
+}
